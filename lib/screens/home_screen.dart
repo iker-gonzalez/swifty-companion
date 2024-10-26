796df25f5ab6daf51611fe29465dfdd76073c55a
Iker Gonzalez <ikergpe@gmail.com>
2024-10-26 12:00:22 +0200

    Fix back arrow crash issue

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -46,29 +46,23 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void _fetchUsersByCampus(String campusName) async {
-    final users = await _apiService.getUsersByCampus(campusName.toLowerCase());
-    setState(() {
-      _usersByCampus = users;
-      _filteredUsers = users;
-    });
-  }
-
   void _login() async {
     final authorizationUrl = _authService.getAuthorizationUrl();
     final result = await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => WebViewPage(
-          initialUrl: authorizationUrl,
-          onCodeReceived: (code) async {
-            final accessToken = await _authService.exchangeCodeForToken(code);
-            if (accessToken != null) {
-              final loggedUserInfo = await _apiService.getLoggedUserInfo();
-              Navigator.pop(context, {'userInfo': loggedUserInfo});
-            }
-          },
-        ),
+        builder: (context) =>
+            WebViewPage(
+              initialUrl: authorizationUrl,
+              onCodeReceived: (code) async {
+                final accessToken = await _authService.exchangeCodeForToken(
+                    code);
+                if (accessToken != null) {
+                  final loggedUserInfo = await _apiService.getLoggedUserInfo();
+                  Navigator.pop(context, {'userInfo': loggedUserInfo});
+                }
+              },
+            ),
       ),
     );
 
@@ -78,107 +72,125 @@ class _HomeScreenState extends State<HomeScreen> {
         _userInfo = result['userInfo'];
       });
 
+      // Update users by campus before navigating
+      if (_userInfo != null) {
+        await _fetchUsersByCampus(_userInfo!.campus);
+      }
+
       // Redirect to logged-in user's UserInfoScreen
-      Navigator.pushReplacement(
+      Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => UserInfoScreen(
-            userId: _userInfo!.id,
-            loggedInUserProfilePicture: _userInfo!.profilePicture,
-          ),
+          builder: (context) =>
+              UserInfoScreen(
+                userId: _userInfo!.id,
+                loggedInUserProfilePicture: _userInfo!.profilePicture,
+              ),
         ),
       );
     }
   }
 
-  void _logout() {
+  // Make _fetchUsersByCampus return a Future
+  Future<void> _fetchUsersByCampus(String campusName) async {
+    final users = await _apiService.getUsersByCampus(campusName.toLowerCase());
     setState(() {
-      _isLoggedIn = false;
-      _userInfo = null;
-      _usersByCampus = null;
-      _filteredUsers = null;
+      _usersByCampus = users;
+      _filteredUsers = users;
     });
   }
 
-  void _goToProfile(int userId) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => UserInfoScreen(
-          userId: userId,
-          loggedInUserProfilePicture: _userInfo?.profilePicture ?? '',
+    void _logout() {
+      setState(() {
+        _isLoggedIn = false;
+        _userInfo = null;
+        _usersByCampus = null;
+        _filteredUsers = null;
+      });
+    }
+
+    void _goToProfile(int userId) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              UserInfoScreen(
+                userId: userId,
+                loggedInUserProfilePicture: _userInfo?.profilePicture ?? '',
+              ),
         ),
-      ),
-    );
-  }
+      );
+    }
 
-  void _filterUsers(String query) {
-    setState(() {
-      _searchQuery = query;
-      _filteredUsers = _usersByCampus
-          ?.where((user) => user.login.toLowerCase().contains(query.toLowerCase()))
-          .toList();
-    });
-  }
+    void _filterUsers(String query) {
+      setState(() {
+        _searchQuery = query;
+        _filteredUsers = _usersByCampus
+            ?.where((user) =>
+            user.login.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      });
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Header(
-        title: 'Home Screen',
-        isLoggedIn: _isLoggedIn,
-        profilePictureUrl: _userInfo?.profilePicture ?? '',
-        onLogout: _logout,
-        onGoToProfile: () => _goToProfile(_userInfo!.id),
-      ),
-      body: Container(
-        color: Colors.grey[200],
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                if (!_isLoggedIn)
-                  ElevatedButton(
-                    onPressed: _login,
-                    child: const Text('Login with OAuth2'),
-                  ),
-                if (_filteredUsers != null) ...[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Search by login',
-                        border: OutlineInputBorder(),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: Header(
+          title: 'Home Screen',
+          isLoggedIn: _isLoggedIn,
+          profilePictureUrl: _userInfo?.profilePicture ?? '',
+          onLogout: _logout,
+          onGoToProfile: () => _goToProfile(_userInfo!.id),
+        ),
+        body: Container(
+          color: Colors.grey[200],
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  if (!_isLoggedIn)
+                    ElevatedButton(
+                      onPressed: _login,
+                      child: const Text('Login with OAuth2'),
+                    ),
+                  if (_filteredUsers != null) ...[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          labelText: 'Search by login',
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: _filterUsers,
                       ),
-                      onChanged: _filterUsers,
                     ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: _filteredUsers!.length,
-                      itemBuilder: (context, index) {
-                        final user = _filteredUsers![index];
-                        return Card(
-                          margin: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: ListTile(
-                            title: Text(
-                              user.login,
-                              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _filteredUsers!.length,
+                        itemBuilder: (context, index) {
+                          final user = _filteredUsers![index];
+                          return Card(
+                            margin: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: ListTile(
+                              title: Text(
+                                user.login,
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              onTap: () => _goToProfile(user.id),
                             ),
-                            onTap: () => _goToProfile(user.id),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
-                  ),
+                  ],
                 ],
-              ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
